982d3b4605b071df94013845977dfbd11f797a89
Paoo12 <paolosalvador28@gmail.com>
2024-12-02 18:54:36 -0700

    student attendance

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+const uri = process.env.MONGO_URI;
+const mongoose = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+//connection logic connect to mongodb
+mongoose.connect(uri).then(
+    async () => {
+
+        console.log(`Connected to MongoDB Database`)
+
+        app.listen(PORT, () => {
+            console.log(`Connected to port ${PORT}`);
+        });
+    }
+).catch((err) => {
+    console.log(`Error connecting to MongoDB Database: ${err}`);
+});
+
+
