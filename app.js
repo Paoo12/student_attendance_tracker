b52c0da0449eaeed37c00e5d9e2cf918a0d837f2
Paoo12 <paolosalvador28@gmail.com>
2024-12-02 19:05:22 -0700

    first update

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const uri = process.env.MONGO_URI;
 const mongoose = require('mongoose');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static('public'));
+
+//swagger documentation
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //connection logic connect to mongodb
 mongoose.connect(uri).then(
